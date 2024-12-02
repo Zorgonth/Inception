15a6ab8bd6e6e4cd5bf13f1cdce8d1c966e67219
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-12-02 12:38:53 +0100

    Some more fixes

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -1,15 +1,15 @@
 #!bin/sh
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
+        chown -R mysql:mysql /var/lib/mysql
+        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+        tfile=`mktemp`
+        if [ ! -f "$tfile" ]; then
+        	return 1
+        fi
 fi
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-	cat << EOF > /tmp/create_db.sql
+	cat << EOF > /tmp/init_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM     mysql.user WHERE User='';
@@ -23,6 +23,6 @@ GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
         # run init.sql
-        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-        rm -f /tmp/create_db.sql
+        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
+        rm -f /tmp/init_db.sql
 fi
--- srcs/requirements/wordpress/tools/make_dir.sh
+++ srcs/requirements/wordpress/tools/make_dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -d "/home/${USER}/data" ]; then
+if [ ! -d "/home/sal-zuba/data" ]; then
         mkdir ~/data
         mkdir ~/data/mariadb
         mkdir ~/data/wordpress
